4c0ba7d9d7b1cb804874208d323db52faa43d3ed
Gabriel Kim <gabrielkim13@gmail.com>
2020-09-05 19:59:34 -0300

    Add SignUp page

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logo from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <h1>Faça seu cadastro</h1>
+
+        <Input name="name" icon={FiUser} placeholder="Nome" />
+        <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Cadastrar</Button>
+
+        <a href="/">Esqueci minha senha</a>
+      </form>
+
+      <a href="/">
+        <FiArrowLeft />
+        Voltar para logon
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;

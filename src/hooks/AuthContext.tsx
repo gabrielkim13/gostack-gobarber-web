53fcb9cf1135f39b4b0d9d28b33d19ff73c678fa
Gabriel Kim <gabrielkim13@gmail.com>
2020-09-06 12:01:21 -0300

    Add authentication context - Add AuthContext context. - Add signIn method with API call to /sessions. - Add user and token information on localStorage. - Refactor AuthContext as a custom hook useAuth.

---
+++ src/hooks/AuthContext.tsx
@@ -0,0 +1,67 @@
+import React, { createContext, useContext, useCallback, useState } from 'react';
+import api from '../services/api';
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthState {
+  user: Record<string, unknown>;
+  token: string;
+}
+
+interface AuthContextData {
+  user: Record<string, unknown>;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const user = localStorage.getItem('@GoBarber:user');
+    const token = localStorage.getItem('@GoBarber:token');
+
+    if (user && token) {
+      return { user: JSON.parse(user), token };
+    }
+
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('/sessions', { email, password });
+
+    const { user, token } = response.data;
+
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+    localStorage.setItem('@GoBarber:token', token);
+
+    setData({ user, token });
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:user');
+    localStorage.removeItem('@GoBarber:token');
+
+    setData({} as AuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within a AuthProvider');
+  }
+
+  return context;
+}
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -6,6 +6,8 @@ import { Form } from '@unform/web';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
+import { useAuth } from '../../hooks/AuthContext';
+
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
@@ -13,29 +15,41 @@ import logo from '../../assets/logo.svg';
 
 import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
+  const { signIn } = useAuth();
+
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: Record<string, unknown>) => {
-    try {
-      formRef.current?.setErrors({});
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
 
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('E-mail obrigatório')
-          .email('Digite um e-mail válido'),
-        password: Yup.string().required('Senha obrigatória'),
-      });
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
 
-      await schema.validate(data, { abortEarly: false });
-    } catch (err) {
-      console.log(err);
+        await schema.validate(data, { abortEarly: false });
 
-      const errors = getValidationErrors(err);
+        signIn({ email: data.email, password: data.password });
+      } catch (err) {
+        console.log(err);
 
-      formRef.current?.setErrors(errors);
-    }
-  }, []);
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+      }
+    },
+    [signIn],
+  );
 
   return (
     <Container>
@@ -43,7 +57,7 @@ const SignIn: React.FC = () => {
         <img src={logo} alt="GoBarber" />
 
         <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu logon</h1>
+          <h1>Faça seu login</h1>
 
           <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
           <Input
